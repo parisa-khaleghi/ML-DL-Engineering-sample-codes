4cf88ae2c3929c6cebd513c0904f20301b3fb3e3
parisa-khaleghi <parisa.khaleghi89@gmail.com>
2023-10-05 23:30:57 +0300

    update: use numpy array instead of python list

--- ML with python/3- Gradient Descent and Cost Function/Gradient Descent and Cost Function.py
+++ ML with python/3- Gradient Descent and Cost Function/Gradient Descent and Cost Function.py
@@ -3,7 +3,8 @@ import numpy as np
 def gradient_decent(x, y):
     pass
 
-x = [1,2,3,4,5]
-y = [5,7,9,11,13]
+# use numpy array instead of python list for convinience
+x = np.array([1,2,3,4,5])
+y = np.array([5,7,9,11,13])
 
 gradient_decent(x, y)
