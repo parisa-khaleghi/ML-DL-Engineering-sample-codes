7dec9e9adb519f239975d2803da6f9b6c2633038
parisa-khaleghi <parisa.khaleghi89@gmail.com>
2023-10-07 21:00:29 +0300

    updateL find learninf rate as 0.0002

--- ML with python/3- Gradient Descent and Cost Function/Exercise.py
+++ ML with python/3- Gradient Descent and Cost Function/Exercise.py
@@ -13,25 +13,18 @@ def gradient_descent(x, y):
 
     m_current = b_current = 0
     n = len(x)
-    # learning_rate =   0.0002 # 31.604511333529594 31.604511362827743
-    learning_rate =   0.0002
-    cost = cost_temp = 0
+    
+    learning_rate =   0.0002 # 31.604511334602297
+    cost = cost_next = 0
 
     for i in range(1000000):
         y_predicted = m_current * x + b_current
+        cost_next = (1/n) * sum ([val**2 for val in (y-y_predicted)])
 
-        if i == 0:
-            cost = (1/n) * sum ([val**2 for val in (y-y_predicted)])
-            cost_temp = cost
-        else:
-            cost_temp = (1/n) * sum ([val**2 for val in (y-y_predicted)])
-            if(cost < cost_temp):
-                print("cost {}, cost_temp {}, iteration {}, <=".format(cost, cost_temp, i))
-                break;
-            if(math.isclose(cost, cost_temp, rel_tol=1e-20, abs_tol=0.0) == True):
-                print("cost {}, cost_temp {}, iteration {}, ==".format(cost, cost_temp, i))
-                break;
-        cost = cost_temp
+        if math.isclose(cost, cost_next, rel_tol=1e-20):
+            print("cost {}, cost_next {}, iteration {}, ==".format(cost, cost_next, i))
+            break;
+        cost = cost_next
         d_m  = -(2/n) * sum(x * (y - y_predicted))
         d_b  = -(2/n) * sum(y - y_predicted)
         m_current = m_current - learning_rate * d_m
